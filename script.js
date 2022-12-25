059613438dc95e424517d9c9e7a3f613458df7be
Abdullahi Salawu <salabdullahi33@gmail.com>
2022-12-25 17:34:10 +0100

    operations

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const mainBtn = document.querySelector('.main-btn')
 const feature = document.querySelector('#feature')
 const openAccount = document.querySelector('.header-list-item-btn')
 const closeModal = document.querySelector('.btn-close-modal')
+const operationTabsCon = document.querySelector('.operations-btn-con')
+const operationsTabs = document.querySelectorAll('.operations-btn')
+const operationContent = document.querySelectorAll('.operations-detail-con')
 
 const modal = document.querySelector('.modal')
 const overlay = document.querySelector('.overlay')
@@ -36,3 +39,34 @@ document.addEventListener('keydown',(e)=>{
         toCloseModal()
     }
 })
+
+//event delegation
+document.querySelector('.header-list').addEventListener('click', (e)=>{
+    if(e.target.classList.contains('nav-link')){
+        e.preventDefault()
+        const id = e.target.getAttribute('href')
+        document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+    }
+})
+
+//operations functinalities
+operationTabsCon.addEventListener('click', (e)=>{
+    const clicked = e.target.closest('.operations-btn')
+
+    //deactivating button
+    operationsTabs.forEach((tab)=>{
+        tab.classList.remove('operation-btn-active')
+    })
+    operationContent.forEach((content)=>{
+        content.classList.remove('operations-detail-active')
+    })
+
+    if(!clicked) return
+    //activating button
+    clicked.classList.add('operation-btn-active')
+    document.querySelector(`.operation-content-${clicked.dataset.tab}`).classList.add('operations-detail-active')
+})
+
+
+
+
